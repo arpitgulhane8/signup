034737aa055cdd86e778c3357e27bbfb40a5e44d
arpitgulhane8 <162834636+arpitgulhane8@users.noreply.github.com>
2024-12-03 02:23:37 +0530

    Update authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URI = "https://signup-4xzw.onrender.com"
+const API_URI = "https://signup-4xzw.onrender.com/api/auth"
 
 export const registerUser = async (userData) =>{
     try{
